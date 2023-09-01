0863d52a729ce8ce211b9211b591e31f2a753e14
Fredrik Eriksson <f.eriksson86@gmail.com>
2023-09-01 21:04:49 +0200

    Init

--- components/Settings.js
+++ components/Settings.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Profile from './Profile';
+import Camera from './Camera';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function Settings() {
+const Tab = createBottomTabNavigator();
+
+function Settings() {
   return (
-    <Text>Settings</Text>
-  )
+    <Tab.Navigator>
+      <Tab.Screen name="Profile" component={Profile}/>
+      <Tab.Screen name="Camera" component={Camera} />
+    </Tab.Navigator>
+  );
 }
+export default Settings
