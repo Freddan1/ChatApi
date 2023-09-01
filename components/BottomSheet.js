2b5b676cef55b3f67116a19a52655f686475a41a
Fredrik Eriksson <f.eriksson86@gmail.com>
2023-09-01 09:38:09 +0200

    :)

--- components/BottomSheet.js
+++ components/BottomSheet.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
 
 const {height: SCREEN_HEIGHT} = Dimensions.get('window');
 
 export default function BottomSheet() {
   return (
     <View style={styles.bottomSheetContainer}>
-      <Text>Vill du ta bort meddelande?</Text>
+      <View style={styles.container}>
+      <AntDesign name="delete" size={24} color="black" />
+      </View>
     </View>
   )
 }
@@ -20,4 +23,10 @@ const styles = StyleSheet.create({
     top: SCREEN_HEIGHT / 1.3,
     borderRadius: 20,
    },
+   container: {
+    flex: 1,
+    alignContent: "center",
+    alignItems: "center",
+    marginTop: 40
+   }
 })
